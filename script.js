b4c361d3a221c9953088438a623ab949630c988d
kleintrevor <trevor.klein@gmail.com>
2022-08-01 04:30:29 -0500

    animated background, merged images while scrolling

--- script.js
+++ script.js
@@ -1,87 +1,32 @@
-let playerState = 'run';
-const dropdown = document.getElementById('animations');
-dropdown.addEventListener('change', function(e){
-  playerState = e.target.value;
-})
-
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 5;
 
-const playerImage = new Image();
-playerImage.src = 'shadow_dog.png';
-const spriteWidth = 575;
-const spriteHeight = 523;
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'images/layer-1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'images/layer-2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'images/layer-3.png';
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'images/layer-4.png';
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'images/layer-5.png';
 
-let gameFrame = 0;
-const staggerFrames = 5;
-const spriteAnimations = [];
-const animationStates = [
-  {
-    name: 'idle',
-    frames: 7,
-  },
-  {
-    name: 'jump',
-    frames: 7,
-  },
-  {
-    name: 'fall',
-    frames: 7,
-  },
-  {
-    name: 'run',
-    frames: 9,
-  },
-  {
-    name: 'dizzy',
-    frames: 11
-  },
-  {
-    name: 'sit',
-    frames: 5,
-  },
-  {
-    name: 'roll',
-    frames: 7,
-  },
-  {
-    name: 'bite',
-    frames: 7,
-  },
-  {
-    name: 'ko',
-    frames: 12,
-  },
-  {
-    name: 'getHit',
-    frames: 4,
-  }
-];
-
-animationStates.forEach((state, index) => {
-  let frames = {
-    loc: [],
-  }
-  for (let j = 0; j < state.frames; j++){
-    let positionX = j * spriteWidth;
-    let positionY = index * spriteHeight;
-    frames.loc.push({x: positionX, y: positionY});
-  }
-  spriteAnimations[state.name] = frames;
-});
+let x = 0;
+let x2 = 2400;
 
 function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
-    let frameX = spriteWidth * position;  //advanced calculation, pulled from internet
-    let frameY = spriteAnimations[playerState].loc[position].y;
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(backgroundLayer4, x, 0);
+  ctx.drawImage(backgroundLayer4, x2, 0);
+  if (x < -2400) x = 2400 + x2 - gameSpeed;
+  else x -= gameSpeed;
+  if (x2 < -2400) x2 = 2400 + x - gameSpeed;
+  else x2 -= gameSpeed;
+  requestAnimationFrame(animate);
+}
 
-    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-
-    gameFrame++;
-    requestAnimationFrame(animate);
-
-};
-animate();
+animate()
