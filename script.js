ed0ee45f1f6c76f3df6a1de83396fa02885d8875
kleintrevor <trevor.klein@gmail.com>
2022-08-02 01:17:53 -0500

    added scroll bar feature

--- script.js
+++ script.js
@@ -15,18 +15,60 @@ backgroundLayer4.src = 'images/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'images/layer-5.png';
 
-let x = 0;
-let x2 = 2400;
+const slider = document.getElementById('slider');
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById('showGameSpeed');
+showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener('change', function(e){
+    console.log(e.target.value);
+    gameSpeed = e.target.value;
+    showGameSpeed.innerHTML = e.target.value;
+  }
+)
+
+class Layer {
+  constructor(image, speedModifier){
+    this.x = 0;
+    this.y = 0;
+    this.width = 2400;
+    this.height = 700;
+    this.x2 = this.width;
+    this.image = image;
+    this.speedModifier = speedModifier;
+    this.speed = gameSpeed * this.speedModifier;
+  }
+  update(){
+    this.speed = gameSpeed * this.speedModifier;
+    if (this.x <= -this.width){
+      this.x = this.width + this.x2 - this.speed
+    }
+    if (this.x2 <= -this.width){
+      this.x2 = this.width + this.x - this.speed
+    }
+    this.x = Math.floor(this.x - this.speed)
+    this.x2 = Math.floor(this.x2 - this.speed)
+  }
+  draw(){
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height)
+  }
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.drawImage(backgroundLayer4, x, 0);
-  ctx.drawImage(backgroundLayer4, x2, 0);
-  if (x < -2400) x = 2400 + x2 - gameSpeed;
-  else x -= gameSpeed;
-  if (x2 < -2400) x2 = 2400 + x - gameSpeed;
-  else x2 -= gameSpeed;
+  gameObjects.forEach(object => {
+    object.update();
+    object.draw();
+  })
   requestAnimationFrame(animate);
-}
+};
 
-animate()
+animate();
